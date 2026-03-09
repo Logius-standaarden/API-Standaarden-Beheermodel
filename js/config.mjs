fc61c9962210f21397114843c5bc4ff6434b19df
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2026-03-09 10:15:21 +0100

    Beheer: verwijder PDF formaat

--- js/config.mjs
+++ js/config.mjs
@@ -1,10 +1,6 @@
 import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
 
 loadRespecWithConfiguration({
-  alternateFormats: [{
-    "label": "pdf",
-    "uri": "API-Standaarden-Beheermodel.pdf"
-  }],
   authors: [{
     "mailto": "Martin.plas@logius.nl",
     "name": "Martin van der Plas",
